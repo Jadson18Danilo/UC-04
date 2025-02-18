7889356fbaabbbdf1a66587ba5e41b6ec637e808
Jadson Danilo <Jadson17danilo@gmail.com>
2025-02-18 22:59:57 +0000

    adicionou o switch

---
+++ Aula02/src/index.js
@@ -0,0 +1,66 @@
+import { AlunoController } from './modulos/Aluno/controllers/index.js';
+import promptSync from 'prompt-sync';
+
+const prompt = promptSync(); 
+
+const aluno1 = new AlunoController();
+
+function exibirMenu() {
+    console.log("\n===== MENU =====");
+    console.log("1 - Criar novo aluno");
+    console.log("2 - Listar todos alunos");
+    console.log("3 - Editar aluno por matrícula");
+    console.log("4 - Excluir todos alunos");
+    console.log("5 - Excluir aluno por matrícula");
+    console.log("0 - Sair");
+}
+
+let controle;
+
+do {
+    exibirMenu();
+    controle = parseInt(prompt("Digite uma numeração de 0 a 5: "), 10);
+
+    switch (controle) {
+        case 1:
+            
+            break;
+
+        case 2:
+            
+            break;
+
+        case 3:
+            
+            break;
+
+        case 4:
+            
+            break;
+
+        case 5:
+            
+            break;
+
+        case 0:
+            
+            break;
+
+        default:
+            console.log("Opção inválida! Digite um número de 0 a 5.");
+            break;
+    }
+
+} while (controle !== 0);
+
+
+aluno1.criar("a9111","Irineu","ireneu@edum.senac.br","@123")
+aluno1.criar("a9222","Borges","borges@edum.senac.br","@123")
+
+aluno1.listarTodos()
+
+aluno1.deletarPorMatricula("a9111")
+aluno1.listarTodos()
+
+aluno1.editar('a9222',null,"borgeseditado@edum.senac.br",null)
+aluno1.listarTodos()
